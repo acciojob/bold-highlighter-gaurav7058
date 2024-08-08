709b577d622c1c58c78ebb221285c1d1de2c4ec1
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-08-08 12:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-function highlight() {
-    //Write your code here
 
+function highlight() {
+  //Write your code here
+let strong=document.querySelectorAll("strong");
+
+strong.forEach(value=>value.style.color="green")
 
 }
 
 
 function return_normal() {
-    //Write your code here
-
-    
+  //Write your code here
+  let strong=document.querySelectorAll("strong");
+  strong.forEach(value=>value.style.color="black")
 }
